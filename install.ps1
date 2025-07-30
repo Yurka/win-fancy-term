2cf1380b223c3da901297f436c63b8d29350cb9a
Yurka <yuriy.rovda@gmail.com>
2025-07-30 22:12:30 +0300

    add scoop

--- install.ps1
+++ install.ps1
@@ -1,10 +1,6 @@
 
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
-try {
-    Invoke-RestMethod -Uri "https://get.scoop.sh" | Invoke-Expression
-} catch {
-    Write-Host "Ошибка при выполнении Invoke-RestMethod, продолжаем работу..." -ForegroundColor Yellow
-}
 
-scoop -v
+scoop install 7zip btop curl docker fastfetch fd firefox fzf gcc git gradle gzip Hack-NF JetBrainsMono-NF jq lf lua luarocks make maven neovim nu openjdk openssh pwsh rapidee ripgrep rustdesk simplenote starship sudo telegram terminal-icons unzip w64devkit windows-terminal zoxide
+
+
