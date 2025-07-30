f4f7b9970e40397fec9bd11e5c63b71ffe585c28
Yurka <yuriy.rovda@gmail.com>
2025-07-30 22:49:00 +0300

    update

--- install.ps1
+++ install.ps1
@@ -6,4 +6,19 @@ scoop bucket add java
 
 scoop install 7zip btop curl docker fastfetch fd fzf gcc git gradle gzip Hack-NF JetBrainsMono-NF jq lf lua luarocks make maven neovim nu openjdk openssh pwsh rapidee ripgrep rustdesk simplenote starship sudo telegram terminal-icons unzip w64devkit windows-terminal zoxide
 
+Add-Content -Path "C:\Users\Yurka\AppData\Roaming\nushell\config.nu" -Value @(
+	'$env.config.show_banner = false',
+	'$env.config.buffer_editor = "nvim"',
+	'',
+	'# ========= end',
+	'',
+	'mkdir ($nu.data-dir | path join "vendor/autoload")',
+	'starship init nu | save -f ($nu.data-dir | path join "vendor/autoload/starship.nu")',
+	'source ~/.zoxide.nu'
+)
 
+Add-Content -Path "C:\Users\Yurka\AppData\Roaming\nushell\env.nu" -Value @(
+	'',
+	'# ========= end',
+	'zoxide init nushell | save -f ~/.zoxide.nu'
+)
