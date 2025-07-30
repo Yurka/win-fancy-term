948661368b7154a93d68c5aba189a4ac998d0c89
Yurka <yuriy.rovda@gmail.com>
2025-07-30 22:26:29 +0300

    update

--- install.ps1
+++ install.ps1
@@ -1,6 +1,9 @@
+scoop bucket add main
+scoop bucket add extras
+scoop bucket add nerd-fonts
+scoop bucket add java
 
 
-
-scoop install 7zip btop curl docker fastfetch fd firefox fzf gcc git gradle gzip Hack-NF JetBrainsMono-NF jq lf lua luarocks make maven neovim nu openjdk openssh pwsh rapidee ripgrep rustdesk simplenote starship sudo telegram terminal-icons unzip w64devkit windows-terminal zoxide
+scoop install 7zip btop curl docker fastfetch fd fzf gcc git gradle gzip Hack-NF JetBrainsMono-NF jq lf lua luarocks make maven neovim nu openjdk openssh pwsh rapidee ripgrep rustdesk simplenote starship sudo telegram terminal-icons unzip w64devkit windows-terminal zoxide
 
 
